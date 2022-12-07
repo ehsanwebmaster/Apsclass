3309b2773d2bc1e6e84c0b1865385be5419aa644
Ehsan Farahmand <89188159+ehsanwebmaster@users.noreply.github.com>
2022-12-07 22:25:38 +0330

    completed

--- Guessmynumber/script.js
+++ Guessmynumber/script.js
@@ -1,1 +1,73 @@
 'use strict';
+// document.querySelector('.check').addEventListener('click', function () {
+//   console.log('Hello');
+// });
+// console.log(typeof(document.querySelector('.guess').value));
+
+let randomNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('⛔ No number!');
+  } 
+  
+  else if (guess === randomNumber) {
+    displayMessage('🧑🏻‍❤️ Correct number');
+    document.body.style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } 
+  
+  else if (guess !== randomNumber) {
+    if (score > 1) {
+      guess < randomNumber ? displayMessage('📉 Too low!'): displayMessage('📈 Too High!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('🤦‍♂️ You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  //   else if (guess < randomNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '📉 Too low!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = '🤦‍♂️ You lost the game!';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   }
+  //   else if (guess > randomNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '📈 Too High!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = '🤦‍♂️ You lost the game!';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  randomNumber = Math.trunc(Math.random() * 20 + 1);
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,24 @@
 'use strict';
+const btnModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const addClass = ()=>{
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+const removeClass = ()=>{
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+for (let i = 0; i < btnModal.length; i++) {
+  btnModal[i].addEventListener('click', removeClass);
+}
+closeModalBtn.addEventListener('click', addClass);
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    addClass();
+  }
+});
+overlay.addEventListener('click', addClass);
